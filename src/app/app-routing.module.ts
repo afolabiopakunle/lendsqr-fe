126d92025eba0866e2d32bffbcbfca26c181d657
Afolabi <opakunleafolabi@gmail.com>
2023-03-18 14:19:10 +0100

    routes implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UsersComponent } from './components/users/users.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 const routes: Routes = [
-  {
-    path: '', redirectTo: 'users', pathMatch: 'full',
-  },
-  {
-    path: 'users', component: UsersComponent,
-  }
+  { path: '',  redirectTo: 'users', pathMatch: 'full' },
+  { path: 'users', component: UsersComponent },
+  { path: 'dashboard', component: DashboardComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,14 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { UsersComponent } from './components/users/users.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
-    UsersComponent
+    UsersComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
