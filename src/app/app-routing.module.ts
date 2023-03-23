897fb209f043a0dfee23953c6021e76907215136
Afolabi <opakunleafolabi@gmail.com>
2023-03-23 09:48:42 +0100

    new routing implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,19 +6,28 @@ import { UserDetailsComponent } from './components/user-details/user-details.com
 import { GurantorsComponent } from './pages/gurantors/gurantors.component';
 import { LoansComponent } from './pages/loans/loans.component';
 import { LoginComponent } from './pages/login/login.component';
+import { LandingComponent } from './landing/landing.component';
 
 const routes: Routes = [
-  { path: '',  redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
-  { path: 'users', component: UsersComponent },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'user-details/:id', component: UserDetailsComponent },
-  { path: 'guarantors', component: GurantorsComponent },
-  { path: 'loans', component: LoansComponent },
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'landing', component: LandingComponent,
+    children: [
+      {path: '', redirectTo: 'users', pathMatch: 'full'},
+      {path: 'users', component: UsersComponent},
+      {path: 'dashboard', component: DashboardComponent },
+      {path: 'user-details/:id', component: UserDetailsComponent},
+      {path: 'guarantors', component: GurantorsComponent},
+      {path: 'loans', component: LoansComponent},
+      {path: 'dashboard', component: DashboardComponent},
+    ],
+  },
+
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
